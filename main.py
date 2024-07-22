5fc24687f92ac55eab13e6b6affccd66b4056df2
jmctune <17505625+jmctune@users.noreply.github.com>
2024-07-22 08:40:04 -0500

    fxtwitter has been broke for about a month with no signs of fixing, so this processes the tweets ourselves and translates them through deepl.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 import datetime
 import os
@@ -6,57 +6,125 @@ import requests
 import sqlite3
 import sys
 import xml.etree.ElementTree as ET
-
-# for testing. make sure you remove before commiting! :)
-TESTING_WEBHOOK_URL = ''
-WEBHOOK_URLS = {
-    'main': os.environ.get('DISCORD_WEBHOOK_MAIN_URL', TESTING_WEBHOOK_URL),
-}
-
-DQX_TWITTER = "https://twiiit.com/DQ_X/rss"
+from bs4 import BeautifulSoup
+import deepl
 
 
-def notify_webhook(content: str):
-    """Triggers a Discord webhook URL."""
-    webhook = DiscordWebhook(
-        url=WEBHOOK_URLS['main'],
-        username=f'[nitter] DQ_X',
-        content=content
+WEBHOOK_URL =  os.environ.get("DISCORD_WEBHOOK_MAIN_URL", "")
+TRANSLATE_KEY = os.environ.get("DEEPL_API_KEY", "")
+
+TWITTER = "http://twiiit.com"  # picks a random, online-only nitter instance
+DQX_TWITTER = f"{TWITTER}/DQ_X"
+RSS_FEED = DQX_TWITTER + "/rss"
+TWEET_STATUS = DQX_TWITTER + "/status"
+
+
+def notify_webhook(content: DiscordEmbed):
+    if WEBHOOK_URL:
+        webhook = DiscordWebhook(url=WEBHOOK_URL)
+        embed = content
+        webhook.add_embed(embed)
+
+        response = webhook.execute()
+    else:
+        response = "No webhook URL configured. Unable to send message to Discord."
+    return response
+
+
+def get_tweet_contents(url: str) -> dict:
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    content = soup.find(class_="tweet-content media-body").text
+    image = soup.find(class_="still-image").get("href")
+    if image:
+        image = TWITTER + image
+    
+    return {
+        "text": content,
+        "image": image
+    }
+
+
+def translate_tweet(text: str) -> str:
+    if not TRANSLATE_KEY:
+        return text
+    translator = deepl.Translator(TRANSLATE_KEY)
+    response = translator.translate_text(
+        text = text,
+        source_lang = "ja",
+        target_lang = "en-us",
+        preserve_formatting = True
     )
 
-    webhook.execute()
+    return response.text
 
 
-db_file = 'state.db'
+db_file = "state.db"
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-response = requests.get(url=DQX_TWITTER)
+response = requests.get(url=RSS_FEED)
 if response.status_code != 200:
-    print(f'Could not get nitter data.')
+    print(f"Could not get nitter data.")
     sys.exit(1)
 
-tweets = []
-rss_data = ET.fromstring(response.text)
-for row in rss_data[0].iter('item'):
-    link = row.find('link').text
-    tweet = link.split('/')[-1].rstrip('#m')
-    tweets.append(tweet)
 
-for tweet in tweets:
+rss_data = ET.fromstring(response.text)
+for row in rss_data[0].iter("item"):
+
+    # get tweet id to see if we've seen it before.
+    link = row.find("link").text
+    nitter_instance = link.split('/')[2]
+    tweet = link.split('/')[-1].rstrip('#m')
+
     query = f"SELECT id FROM tweets WHERE id = {tweet}"
     cursor.execute(query)
     results = cursor.fetchone()
 
     if not results:
-        cur_time = datetime.datetime.now()
+        print(f"New tweet: {tweet}")
 
-        fx_link = f'https://fxtwitter.com/DQ_X/status/{tweet}/en'
+        # get the tweet image first (if it exists)
+        tweet_description = row.find("description").text
+        soup = BeautifulSoup(tweet_description, "html.parser")
 
-        insert = f"INSERT INTO tweets (date, id, link) VALUES ('{cur_time}', {tweet}, '{fx_link}')"
-        cursor.execute(insert)
-        conn.commit()
+        image = soup.img.get('src')
+        if image:
+            # "x" (twitter) images link from this domain. will be more reliable than nitter.
+            image = "https://pbs.twimg.com/media/" + image.split('%2F')[-1] + "?format=jpg"
+            print(f"Tweet has image: {image}")
 
-        if WEBHOOK_URLS['main']:
-            print(f'Posting webhook for {fx_link}.')
-            notify_webhook(content=fx_link)
+        # get the tweet text
+        tweet_text = row.find('title').text
+
+        print("Translating text...")
+        trl_text = translate_tweet(tweet_text)
+
+        tweet_url = f"https://x.com/DQ_X/status/{tweet}"
+
+        embed = DiscordEmbed(
+            title = "Dragon Quest X Official (@DQ_X)",
+            url = tweet_url,
+            description = trl_text,
+        )
+        embed.set_author(
+            name = "twitter_notifier",
+            url = "https://github.com/dqx-translation-project/twitter-notifier"
+        )
+        if image:
+            embed.set_image(url=image)
+
+        print("Posting to webhook...")
+        response = notify_webhook(content=embed)
+
+        # only insert into the db if we were able to notify Discord. otherwise, we will try again next run.
+        if response.status_code == 200:
+            cur_time = datetime.datetime.now()
+            insert = f"INSERT INTO tweets (date, id, link) VALUES ('{cur_time}', {tweet}, '{tweet_url}')"
+            cursor.execute(insert)
+            conn.commit()
+
+print(f"Job completed on nitter instance: {nitter_instance}")
+cursor.close()
